efcd3bebefba489e21e525800835e44455ef24f3
Barcode <88603393+Actuallybarcode@users.noreply.github.com>
2023-02-06 23:39:02 -0600

    Add files via upload

---
+++ client-main/script.js
@@ -0,0 +1,120 @@
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
+
+let loadInterval
+/*/ Credits: ActuallyBarcode/*/
+function loader(element) {
+    element.textContent = ''
+
+    loadInterval = setInterval(() => {
+
+        element.textContent += '.';
+
+
+        if (element.textContent === '....') {
+            element.textContent = '';
+        }
+    }, 300);
+}
+/*/ Credits: ActuallyBarcode/*/
+function typeText(element, text) {
+    let index = 0
+
+    let interval = setInterval(() => {
+        if (index < text.length) {
+            element.innerHTML += text.charAt(index)
+            index++
+        } else {
+            clearInterval(interval)
+        }
+    }, 20)
+}
+
+function generateUniqueId() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+/*/ Credits: ActuallyBarcode/*/
+function chatStripe(isAi, value, uniqueId) {
+    return (
+        `
+        <div class="wrapper ${isAi && 'ai'}">
+            <div class="chat">
+                <div class="profile">
+                    <img 
+                      src=${isAi ? bot : user} 
+                      alt="${isAi ? 'bot' : 'user'}" 
+                    />
+                </div>
+                <div class="message" id=${uniqueId}>${value}</div>
+            </div>
+        </div>
+    `
+    )
+}
+/*/ Credits: ActuallyBarcode/*/
+const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const data = new FormData(form)
+
+    // user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+
+    // to clear the textarea input 
+    form.reset()
+
+    // bot's chatstripe
+    const uniqueId = generateUniqueId()
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+
+    // to focus scroll to the bottom 
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+
+    // specific message div 
+    const messageDiv = document.getElementById(uniqueId)
+
+    // messageDiv.innerHTML = "..."
+    loader(messageDiv)
+
+/*/ Credits: ActuallyBarcode/*/
+    // fetch data from server -> bot's response
+    const response = await fetch('http://localhost:5000', {
+        method: 'POST',
+        headers: {
+            'CONTENT-TYPE': 'application/json'
+        },
+        body: JSON.stringify({
+            prompt: data.get('prompt')
+        })
+    })
+
+    clearInterval(loadInterval);
+    messageDiv.innerHTML = '';
+
+    if(response.ok) {
+        const data = await response.json();
+        const parsedData = data.bot.trim();
+/*/ Credits: ActuallyBarcode/*/
+        typeText(messageDiv, parsedData);
+    } else {
+        const err = await response.text();
+
+        messageDiv.innerHTML = "Something Went Wrong";
+
+        alert(err);
+    }
+}
+
+form.addEventListener('submit', handleSubmit)
+form.addEventListener('keyup', (e) => {
+    if (e.keyCode === 13) {
+        handleSubmit(e)
+    }
+})
